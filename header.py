899e3db46fa892b65cfd35e05a4a0858de876c85
tamama9527 <deus85072@mail.fcu.edu.tw>
2017-02-15 15:53:14 +0800

    防登入

--- header.py
+++ header.py
@@ -1,36 +1,41 @@
 header_info = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
-    'Origin': 'http://service105.sds.fcu.edu.tw',
-    'Connection': 'keep-alive',
-    'Referer': 'http://service105.sds.fcu.edu.tw',
-    'Content-Type': 'application/x-www-form-urlencoded'
+'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+'Origin':'http://service105.sds.fcu.edu.tw',
+'Connection':'keep-alive',
+'Referer':'http://service105.sds.fcu.edu.tw',
+'Content-Type':'application/x-www-form-urlencoded'
+  }
+header_info2={
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+'Accept-Encoding':'gzip, deflate',
+'Accept-Language':'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-CN;q=0.2',
+'Connection':'keep-alive',
+'Content-Type':'application/x-www-form-urlencoded',
 }
-header_info2 = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-CN;q=0.2',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/x-www-form-urlencoded',
+header_info3={
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+'Accept-Encoding':'gzip, deflate',
+'Accept-Language':'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-CN;q=0.2,ja;q=0.2',
+'Cache-Control':'no-cache',
+'Connection':'keep-alive',
+'Content-Type':'application/x-www-form-urlencoded',
+'DNT':'1',
+'Origin':'http://service101.sds.fcu.edu.tw',
+'Pragma':'no-cache',
+'Upgrade-Insecure-Requests':'1',
+'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
 }
-header_info3 = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-CN;q=0.2,ja;q=0.2',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'DNT': '1',
-    'Origin': 'http://service101.sds.fcu.edu.tw',
-    'Pragma': 'no-cache',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
-}
-post_pop = [
-    'ctl00$MainContent$TabContainer1$tabMsg$btnQuery',
-    'ctl00$btnLogout',
-    'ctl00$MainContent$TabContainer1$tabSelected$gvWishList$ctl02$btnQuota',
-    'ctl00$MainContent$TabContainer1$tabSelected$btnGetSub',
-    'ctl00$MainContent$TabContainer1$tabSelected$printButton',
-    'ctl00$MainContent$TabContainer1$tabCourseSearch$wcCourseSearch$btnSearchOther',
-    'ctl00$MainContent$TabContainer1$tabCourseSearch$wcCourseSearch$btnSearch'
-]
+post_pop=[
+                  'ctl00$MainContent$TabContainer1$tabMsg$btnQuery',\
+                  'ctl00$btnLogout',\
+                  'ctl00$MainContent$TabContainer1$tabSelected$gvWishList$ctl02$btnQuota',\
+                  'ctl00$MainContent$TabContainer1$tabSelected$btnGetSub',\
+                  'ctl00$MainContent$TabContainer1$tabSelected$printButton',\
+                  'ctl00$MainContent$TabContainer1$tabCourseSearch$wcCourseSearch$btnSearchOther',\
+                  'ctl00$MainContent$TabContainer1$tabCourseSearch$wcCourseSearch$btnSearch'\
+                  ]
+#                  
+account='D000000'
+passwd='XXXX'
+firstchoose=['XXXX']
+autodrop=[]
