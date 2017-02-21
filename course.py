7e14166e1c95c320eacb3be7671cda675f106a41
vongola12324 <vongola12324@coder.tw>
2017-02-21 11:34:15 +0800

    Reformat file and remove extra comment

--- course.py
+++ course.py
@@ -7,15 +7,13 @@ from datetime import datetime
 import requests
 import copy
 import random
-import sys
 from BeautifulSoup import BeautifulSoup
-import time
 
 
 def login():
     global choose, sample, class_post, config
     class_post = {}
-    postdata = {}
+    post_data = {}
     url_last = None
     msg = None
     while True:
@@ -26,18 +24,16 @@ def login():
             print"something error"
         captcha = random.randint(1000, 9999)
         for e in soup.findAll('input', {'value': True}):
-            postdata[str(e['name'].encode('utf-8'))] = str(e['value'].encode('utf-8'))
-        postdata['ctl00$Login1$vcode'] = str(captcha)
-        postdata['ctl00$Login1$UserName'] = config[u"account"]
-        postdata['ctl00$Login1$Password'] = config[u"passwd"]
-        postdata['ctl00$Login1$RadioButtonList1'] = 'zh-tw'
+            post_data[str(e['name'].encode('utf-8'))] = str(e['value'].encode('utf-8'))
+        post_data['ctl00$Login1$vcode'] = str(captcha)
+        post_data['ctl00$Login1$UserName'] = config[u"account"]
+        post_data['ctl00$Login1$Password'] = config[u"passwd"]
+        post_data['ctl00$Login1$RadioButtonList1'] = 'zh-tw'
         cookies = {'CheckCode': str(captcha)}
-        r = s.post(url=url, data=postdata, headers=header.header_info, cookies=cookies)
+        r = s.post(url=url, data=post_data, headers=header.header_info, cookies=cookies)
         header.header_info3['Origin'] = str(r.url[:32]).encode('utf-8')
         header.header_info2['Host'] = str(r.url[7:32]).encode('utf-8')
         header.header_info3['Referer'] = str(r.url).encode('utf-8')
-        # print r.text
-        # try:
         class_soup = BeautifulSoup(r.text)
         for test in class_soup.findAll('form', {'name': 'aspnetForm', 'id': 'aspnetForm'}):
             url_last = test['action']
@@ -50,7 +46,7 @@ def login():
         try:
             choose = r.url.split('?guid=')[0] + url_last
             pass
-        except AttributeError:
+        except:
             class_soup = BeautifulSoup(r.text)
             msg = class_soup.find('span', {'class': 'msg B1'})
         if r.text.find(u'驗證碼錯誤') == -1 and r.text.find(u'帳號或密碼錯誤') == -1 and r.text.find(u'重新登入') == -1:
@@ -91,22 +87,19 @@ def check_exist():
         r.encoding = 'utf-8'
         class_soup = BeautifulSoup(r.text)
         if class_soup.find('p') is not None:
-            print "你已經有 " + code + " 這堂課了！"
+            print "你已經有 " + code.encode('utf-8') + " 這堂課了！"
             temp.pop(temp.index(code))
-            test_login = class_soup.find('span', {'class': 'msg B1'})
+            # test_login = class_soup.find('span', {'class': 'msg B1'})
     print "檢查完畢"
 
 
-def getclass():
-    global class_post, realcode, temp
-    realcode = copy.deepcopy(temp)
-    auto = config[u"autodrop"]
+def get_class():
+    global class_post, real_code, temp
+    real_code = copy.deepcopy(temp)
     test_login = None
-    count = 0
-    error = 0
     class_post = {}
-    while len(realcode) != 0:
-        for code in realcode:
+    while len(real_code) != 0:
+        for code in real_code:
             r = s.get(url=choose, headers=header.header_info)
             class_soup = BeautifulSoup(r.text)
             for e in class_soup.findAll('input', {'value': True}):
@@ -118,7 +111,6 @@ def getclass():
             class_post['ctl00$MainContent$TabContainer1$tabSelected$btnGetSub'] = '查詢'
             class_post['ctl00$MainContent$TabContainer1$tabSelected$tbSubID'] = code
             class_post['ctl00_ToolkitScriptManager1_HiddenField'] = ''
-            # print class_post
             r = s.post(url=choose, headers=header.header_info2, data=class_post)
             # 餘額檢查
             class_post.clear()
@@ -137,12 +129,11 @@ def getclass():
             try:
                 number = class_soup.find('script', text=re.compile("^setTimeout"))
                 number = number.split(u'：')[1].split('/')[0]
-                # setTimeout("alert('剩餘名額/開放名額：1  / 78 ')",200);
                 print str(pytz.timezone('Asia/Taipei').fromutc(datetime.utcnow())).split('.')[0].encode('utf-8') + ' ',
                 print ' 選課代碼: ' + str(code).encode('utf-8'),
                 print ' 剩餘人數: ' + str(number).encode('utf-8'),
-                print [x.encode('utf-8') for x in realcode]
-            except AttributeError:
+                print [x.encode('utf-8') for x in real_code]
+            except:
                 test_login = class_soup.find('span', {'class': 'msg B1'})
                 print code
                 break
@@ -162,7 +153,7 @@ def getclass():
                     check_msg = class_soup.find('span', {'class': 'msg B1'})
                     if check_msg.contents[0] != u'本科目名額目前已額滿 !':
                         print '你已經選到 ' + code + '，請到課表檢查。'
-                        realcode.pop(realcode.index(code))
+                        real_code.pop(real_code.index(code))
         if test_login is not None:
             if test_login.contents[0] == '您已經在其它地方登入':
                 print '您已經在其它地方登入，嘗試幫你重新登入'
@@ -186,7 +177,7 @@ if __name__ == '__main__':
     choose = None
     sample = None
     config = None
-    realcode = None
+    real_code = None
     temp = None
     with open('config.json') as fin:
         config = json.load(fin)
@@ -203,12 +194,10 @@ if __name__ == '__main__':
                 print '連線逾時，嘗試重新登入'
             try:
                 check_exist()
-                if getclass():
+                if get_class():
                     break
             except KeyboardInterrupt:
                 print "Bye"
                 break
             else:
                 print '選課完畢！'
-
-
